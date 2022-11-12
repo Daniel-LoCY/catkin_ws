77ccae5d1309931dd925618e5dce0f37f8582fd7
Daniel-LoCY <105539301+Daniel-LoCY@users.noreply.github.com>
2022-11-12 15:56:00 +0800

    Update ros_install.sh

--- ros_install.sh
+++ ros_install.sh
@@ -1,7 +1,8 @@
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt install curl -y
-curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+apt update sudo -y
 sudo apt update -y
+sudo apt install curl -y
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt install ros-noetic-desktop -y
 sudo apt install python3-rosdep -y
 sudo apt install python3-rosinstall -y
